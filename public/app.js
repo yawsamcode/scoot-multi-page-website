a6495a61fdc7ba3887f01cbf03c7108b7731191c
yawsamcode <samuelacheampong108@gmail.com>
2024-03-02 16:50:48 +0000

    Added Responsive navigation

---
+++ public/app.js
@@ -0,0 +1,9 @@
+const openBtn = document.getElementById('open-btn');
+const closeBtn = document.getElementById('close-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+
+openBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden'); 
+    console.log('you clicked')
+})
+
