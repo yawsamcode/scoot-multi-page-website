75035a61e08eef0da1922eb4db2b4f434f5038ed
yawsamcode <samuelacheampong108@gmail.com>
2024-03-12 12:45:20 +0000

    Completed

--- public/app.js
+++ public/app.js
@@ -6,3 +6,29 @@ openBtn.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
   console.log("you clicked");
 });
+
+
+
+// ********ACCORDION*******
+// const items = document.querySelectorAll("#accordion .item .header");
+
+// items.forEach((item) => {
+//   item.addEventListener("click", (e) => {
+//     e.currentTarget.closest(".item").classList.toggle("active");
+//   });
+// });
+
+let items = document.querySelectorAll("#accordion .item .header");
+
+  items.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      items.forEach((header) => {
+        const closestItem = header.closest(".item");
+        if (closestItem !== e.currentTarget.closest(".item")) {
+          closestItem.classList.remove("active");
+        }
+      });
+
+      e.currentTarget.closest(".item").classList.toggle("active");
+    });
+  });
